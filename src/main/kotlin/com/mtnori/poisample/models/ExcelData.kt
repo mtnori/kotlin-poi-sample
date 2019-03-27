8e71fa5392cbd3874446aa4c75272191042974db
mtnori <moto44.noriyuki@gmail.com>
2019-03-27 22:05:59 +0900

    関数を追加

--- src/main/kotlin/com/mtnori/poisample/models/ExcelData.kt
+++ src/main/kotlin/com/mtnori/poisample/models/ExcelData.kt
@@ -1,24 +1,15 @@
 package com.mtnori.poisample.models
 
 import org.apache.poi.ss.usermodel.*
+import org.apache.poi.ss.util.CellRangeAddress
 import org.apache.poi.ss.util.CellReference
 import java.io.OutputStream
 
 /**
  * Excelデータを表現するモデルクラス
  */
-class ExcelData {
-    private val workbook: Workbook
-    private var sheet: Sheet
-
-    /**
-     * @param workbook ワークブック
-     * @param sheetIdx シート番号
-     */
-    constructor(workbook: Workbook, sheetIdx: Int = 0) {
-        this.workbook = workbook
-        this.sheet = workbook.getSheetAt(sheetIdx)
-    }
+class ExcelData (private val workbook: Workbook, sheetIdx: Int = 0) {
+    private var sheet: Sheet = workbook.getSheetAt(sheetIdx)
 
     /**
      * ワークブックを保存する
@@ -47,7 +38,7 @@ class ExcelData {
      */
     private fun getCell(rowIdx: Int, colIdx: Int): Cell {
         val row: Row = getRow(rowIdx)
-        val cell: Cell? = row.getCell(colIdx);
+        val cell: Cell? = row.getCell(colIdx)
         if (cell != null) {
             return cell
         }
@@ -71,9 +62,9 @@ class ExcelData {
      * 名前定義からセルを探して文字列をセットする
      */
     fun writeCellByName(value: String, name: String) {
-        val name: Name? = this.workbook.getName(name)
-        if (name !== null) {
-            val ref: CellReference = CellReference(name.refersToFormula)
+        val lName = this.workbook.getName(name)
+        if (lName !== null) {
+            val ref = CellReference(lName.refersToFormula)
             writeCell(value, ref.row, ref.col.toInt())
         }
     }
@@ -89,5 +80,139 @@ class ExcelData {
         val cell: Cell = getCell(rowIdx, colIdx)
         // セルの値を設定する
         cell.cellFormula = value
+    }
+
+    /**
+     * セルを結合する
+     * @param startRowIdx 開始行
+     * @param mergeRowNum 行結合数
+     * @param startColIdx 開始列
+     * @param mergeColNum 列結合数
+     */
+    fun mergeCells(startRowIdx: Int, mergeRowNum: Int, startColIdx: Int, mergeColNum: Int) {
+        this.sheet.addMergedRegion(CellRangeAddress(
+                startRowIdx,
+                startRowIdx + mergeRowNum - 1,
+                startColIdx,
+                startColIdx + mergeColNum - 1))
+    }
+
+    /**
+     * セルの結合を解除する
+     * この処理は遅いので、mergeCells関数内に組み込まないことにする
+     * @param startRowIdx 開始行
+     * @param mergeRowNum 行結合数
+     * @param startColIdx 開始列
+     * @param mergeColNum 列結合数
+     */
+    fun unmergeCells(startRowIdx: Int, mergeRowNum: Int, startColIdx: Int, mergeColNum: Int) {
+        val targetRange = CellRangeAddress(
+                startRowIdx,
+                startRowIdx + mergeRowNum - 1,
+                startColIdx,
+                startColIdx + mergeColNum - 1)
+        val mergeList: List<CellRangeAddress> = this.sheet.mergedRegions
+        val removeIndices: MutableList<Int> = mutableListOf()
+        for ((mergeIndex, mergedAddress) in mergeList.withIndex()) {
+            if (targetRange.intersects(mergedAddress)) {
+                removeIndices.add(mergeIndex)
+            }
+        }
+        // 手前から解除するとインデックスが合わなくなるので後ろから消す
+        removeIndices.reverse()
+        for (removeIndex in removeIndices) {
+            this.sheet.removeMergedRegion(removeIndex)
+        }
+    }
+
+    /**
+     * セルの計算式の計算結果を取得する
+     * @param cell セル
+     * @return セルの計算式の計算結果(文字列
+     */
+    private fun getStringFormulaValue(cell: Cell): String {
+        val helper: CreationHelper = this.workbook.creationHelper
+        val evaluator: FormulaEvaluator = helper.createFormulaEvaluator()
+        val value: CellValue = evaluator.evaluate(cell)
+        when(value.cellType) {
+            CellType.STRING -> {
+                return cell.stringCellValue
+            }
+            CellType.NUMERIC -> {
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    return cell.dateCellValue.toString()
+                }
+                return cell.numericCellValue.toString()
+            }
+            CellType.BOOLEAN -> {
+                return cell.booleanCellValue.toString()
+            }
+            else -> {
+                return ""
+            }
+        }
+    }
+
+    /**
+     * 結合セルの値を取得する
+     * 結合セルの場合、値が入っているのは一番左上のセルになる
+     * @param cell セル
+     * @return セルの値(文字列)
+     */
+    private fun getStringRangeValue(cell: Cell): String {
+        val rowIdx = cell.rowIndex
+        val colIdx = cell.columnIndex
+        val size: Int = sheet.numMergedRegions
+        for (i in 0..size) {
+            val range: CellRangeAddress = sheet.getMergedRegion(i)
+            if (range.isInRange(rowIdx, colIdx)) {
+                return getCellValue(range.firstRow, range.firstColumn)
+            }
+        }
+        return ""
+    }
+
+    /**
+     * セルの値を文字列で返す
+     * @param rowIdx 行番号
+     * @param colIdx 列番号
+     * @return セルの値(文字列)
+     */
+    fun getCellValue(rowIdx: Int, colIdx: Int): String {
+        val cell: Cell = getCell(rowIdx, colIdx)
+        when(cell.cellType) {
+            CellType.STRING -> {
+                return cell.stringCellValue
+            }
+            CellType.NUMERIC -> {
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    return cell.dateCellValue.toString()
+                }
+                return cell.numericCellValue.toString()
+            }
+            CellType.BOOLEAN -> {
+                return cell.booleanCellValue.toString()
+            }
+            CellType.FORMULA -> {
+                // return cell.cellFormula
+                if (cell.cachedFormulaResultType == CellType.ERROR) {
+                    val errorCode = cell.errorCellValue
+                    val error: FormulaError = FormulaError.forInt(errorCode)
+                    return error.string
+                }
+                return getStringFormulaValue(cell)
+            }
+            CellType.ERROR -> {
+                val errorCode = cell.errorCellValue
+                val error: FormulaError = FormulaError.forInt(errorCode)
+                return error.string
+            }
+            CellType.BLANK -> {
+                return getStringRangeValue(cell)
+            }
+            else -> {
+                return ""
+            }
+        }
     }
 }
--- src/main/kotlin/com/mtnori/poisample/services/TestDocumentService.kt
+++ src/main/kotlin/com/mtnori/poisample/services/TestDocumentService.kt
@@ -13,10 +13,27 @@ class TestDocumentService {
     fun create() {
         val excelData = documentRepository.load("test.xlsx")
         if (excelData !== null) {
+            // 既存セルに値をセットする
             excelData.writeCell("cell", 0, 0)
+            // 新規セルに値をセットする
             excelData.writeCell("newCell", 3, 4)
+            // 計算式をセットする
             excelData.writeCellFormula("MOD(10, 3)", 2, 2)
+            // 名前定義からセルを特定し値をセットする
             excelData.writeCellByName("名前定義", "cell_name")
+            // セルの結合
+            excelData.mergeCells(15, 2, 0, 3)
+            // セルの結合(結合されているセルは再結合できないので、結合を解除する)
+            excelData.unmergeCells(7, 2, 0, 3)
+            excelData.mergeCells(7, 2, 0, 3)
+            // セルの値を取得する
+            println(excelData.getCellValue(19,0)) // 文字列
+            println(excelData.getCellValue(20,0)) // 数値
+            println(excelData.getCellValue(21,0)) // 真偽値
+            println(excelData.getCellValue(22,0)) // 計算結果
+            println(excelData.getCellValue(23,0)) // エラー(上手く出ない)
+            println(excelData.getCellValue(25,1)) // 結合セル
+            println(excelData.getCellValue(26,0)) // 日付
             documentRepository.save(excelData, "output.xlsx")
         }
     }
